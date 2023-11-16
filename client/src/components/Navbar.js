cb996f4b60f615efb53ef904df3273e706fe5235
rahulmandal0506 <150768643+rahulmandal0506@users.noreply.github.com>
2023-11-16 13:17:12 +0530

    Add files via upload

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,172 @@
+import { useTheme } from "@emotion/react";
+import {
+  Avatar,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+  Button,
+  InputAdornment,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import "react-icons/ai";
+import "react-icons/ri";
+import Logo from "./img/logo.jpeg";
+import {
+  AiFillFileText,
+  AiFillHome,
+  AiFillMessage,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import { isLoggedIn, logoutUser } from "../helpers/authHelper";
+import UserAvatar from "./UserAvatar";
+import HorizontalStack from "./util/HorizontalStack";
+import { RiContrast2Line } from "react-icons/ri";
+import "./Navbar.css";
+const Navbar = () => {
+  const navigate = useNavigate();
+  const user = isLoggedIn();
+  const theme = useTheme();
+  const username = user && isLoggedIn().username;
+  const [search, setSearch] = useState("");
+  const [searchIcon, setSearchIcon] = useState(false);
+  const [width, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    updateDimensions();
+
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  const mobile = width < 500;
+  const navbarWidth = width < 600;
+
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+  };
+
+  const handleLogout = async (e) => {
+    logoutUser();
+    navigate("/login");
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/search?" + new URLSearchParams({ search }));
+  };
+
+  const handleSearchIcon = (e) => {
+    setSearchIcon(!searchIcon);
+  };
+
+  return (
+    <Stack mb={2}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{
+          pt: 2,
+          pb: 0,
+        }}
+        spacing={!mobile ? 2 : 0}
+      >
+        <HorizontalStack>
+          <img
+            src={Logo}
+            alt=""
+            size={33}
+            color={theme.palette.primary.main}
+            onClick={() => navigate("/")}
+            style={{ width: "45px", height: "auto" }}
+          />
+          <Typography
+            sx={{ display: mobile ? "none" : "block" }}
+            variant={navbarWidth ? "h6" : "h4"}
+            mr={1}
+            color={theme.palette.primary.main}
+          >
+            {/* <Link to="/" color="inherit"> */}
+            Stories
+            {/* </Link> */}
+          </Typography>
+        </HorizontalStack>
+
+        {!navbarWidth && (
+          <Box component="form" onSubmit={handleSubmit}>
+            <TextField
+              size="small"
+              label="Search for posts..."
+              sx={{ flexGrow: 1, maxWidth: 300 }}
+              onChange={handleChange}
+              value={search}
+            />
+          </Box>
+        )}
+
+        <HorizontalStack>
+          {mobile && (
+            <IconButton onClick={handleSearchIcon}>
+              <AiOutlineSearch />
+            </IconButton>
+          )}
+
+          <IconButton component={Link} to={"/"}>
+            <AiFillHome />
+          </IconButton>
+          {user ? (
+            <>
+              <IconButton component={Link} to={"/messenger"}>
+                <AiFillMessage />
+              </IconButton>
+              <IconButton component={Link} to={"/users/" + username}>
+                <UserAvatar width={30} height={30} username={user.username} />
+              </IconButton>
+             
+                <Button onClick={handleLogout} style={{ color: "#f9f9f9",
+  backgroundColor: "#0063e5",
+  marginBottom: "2px",
+  width: "100%",
+  letterSpacing: "1.5px",
+  fontSize: "14px",
+  padding: "4px 2px",
+  border: "1px solid transparent",
+  borderRadius: "4px"}}>Logout</Button>
+              
+            </>
+          ) : (
+            <>
+              <Button variant="text" sx={{ minWidth: 80 }} href="/signup">
+                Sign Up
+              </Button>
+              <Button variant="text" sx={{ minWidth: 65 }} href="/login">
+                Login
+              </Button>
+            </>
+          )}
+        </HorizontalStack>
+      </Stack>
+      {navbarWidth && searchIcon && (
+        <Box component="form" onSubmit={handleSubmit} mt={2}>
+          <TextField
+            size="small"
+            label="Search for posts..."
+            fullWidth
+            onChange={handleChange}
+            value={search}
+          />
+        </Box>
+      )}
+    </Stack>
+  );
+};
+
+export default Navbar;
